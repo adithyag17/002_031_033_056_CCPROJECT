9cbf3648352b8b9cd550c8c7efe1506b66b418a5
Sakthe-Balan <sakthebalan2003@gmail.com>
2024-04-01 11:17:54 +0530

    added changes

---
+++ copy.bat
@@ -0,0 +1,3 @@
+copy C:\GITHUB\cc_proj\002_031_033_056_CCPROJECT\uc1\.env C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc1
+copy C:\GITHUB\cc_proj\002_031_033_056_CCPROJECT\uc1\.env C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc2
+copy C:\GITHUB\cc_proj\002_031_033_056_CCPROJECT\uc1\.env C:\ProgramData\Jenkins\.jenkins\workspace\cc_demo\uc3
